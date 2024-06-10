764ac2bd505a1847431ef04b3e13ded261f481cd
Audrey <audrey.dedieu@beecoming.fr>
2024-06-10 16:53:09 +0200

    Update alt attributes for images

--- client/src/data/HomeData.js
+++ client/src/data/HomeData.js
@@ -3,7 +3,8 @@ export const blockData = [
     title:
       "Bienvenue au Com d Roy, votre évasion culinaire au cœur d'Andernos-les-Bains",
     text: "Niché sur le splendide Bassin d’Arcachon, le Com d Roy vous invite à découvrir le charme d’un café-restaurant où tradition et générosité se rencontrent dans une atmosphère décontractée.",
-    imageUrl: process.env.PUBLIC_URL + "/asset/photos/accueil-ligne1-2.jpg",
+    imageUrl: process.env.PUBLIC_URL + "/asset/photos/table-dressee.jpg",
+    alt: "Table dressée - Un coin intime d'un restaurant avec une table dressée pour deux, entourée de chaises en cuir rouge, près d'une grande fenêtre.",
     imageRight: false,
     showBackground: true,
     backgroundSize: "small",
@@ -11,7 +12,8 @@ export const blockData = [
   {
     title: "Une expérience unique",
     text: "Notre cuisine, un véritable hymne à la tradition culinaire française, vous propose des plats faits maison, élaborés avec amour et savoir-faire, disponibles midi et soir, à la carte ou en formule.",
-    imageUrl: process.env.PUBLIC_URL + "/asset/photos/contact-slide5.jpg",
+    imageUrl: process.env.PUBLIC_URL + "/asset/photos/plat-de-resistance-a-table.jpg",
+    alt: "Plat de résistance : Un plat de poisson servi avec des légumes verts et une sauce, accompagné de vin blanc, dans une présentation soignée sur une table de restaurant.",
     imageRight: true,
     showBackground: true,
     backgroundSize: "medium",
